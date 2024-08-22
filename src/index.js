ff0b3af80876003a34e754c11e316f66d479621e
AryaEm <ary4maulanaa@gmail.com>
2024-08-22 08:28:47 +0700

    update

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ app.use(`/bangun-ruang`, bangunRuangRoute);
 const convertRoute = require(`./routes/suhuConverter`);
 app.use(`/convert`, convertRoute);
 
+const bilangan = require('./routes/bilangan')
+app.use('/bilangan', bilangan)
+
+const bmi = require('./routes/bmi')
+app.use('/bmi', bmi)
+
 app.listen(4000, () => {
   console.log("Server run on port 4000");
 });
